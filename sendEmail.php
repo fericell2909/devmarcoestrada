cc47747acb85fdb05bfdef4f94d209fad429e6e0
Marco Estrada <info@devmarcoestrada.com>
2020-08-09 18:59:19 -0500

    email send

--- sendEmail.php
+++ sendEmail.php
@@ -1,54 +1,67 @@
 <?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
 $data = [];
-if ($_POST) {
-    $name = "";
-    //$phone = "";
-    $email = "";
-    //$subject = "";
-    $comments = "";
-    $recipient="info@devmarcoestrada.com"; // Your email comes here
-
-    if (isset($_POST['name'])) {
-        $name = $_POST['name'];
-    }
-    
-    // if (isset($_POST['phone'])) {
-    //     $phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
-    // }
-
-    if (isset($_POST['email'])) {
-        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    }
-
-    // if (isset($_POST['subject'])) {
-    //     $subject = $_POST['subject'];
-    // }
-
-    if (isset($_POST['comments'])) {
-        $comment = htmlspecialchars($_POST['comments']);
-        $comments = $comment.'<br>Name is: '.$name/*.'<br>Phone number is: '.$phone*/;
-    }
 
 
-    $headers = 'MIME-Version: 1.0' . "\r\n"
-        . 'Content-type: text/html; charset=utf-8' . "\r\n"
-        . 'From: ' . $email . "\r\n";
-    if (mail($recipient, /*$subject,*/ $comments, $headers)) {
-        $data = array(
-            'status' => 'Congratulation',
-            'message' => 'Your message sent successfully.'
-        );
-    } else {
-        $data = array(
-            'status' => 'Error',
-            'message' => 'Your message did not send.'
-        );
-    }
-} else {
-	$data = array(
-		'status' => 'Warning',
-		'message' => 'Something went wrong, Please try again.'
-	);
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
 }
+
+
+if (isset($_POST['email'])) {
+    $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+
+if (isset($_POST['comments'])) {
+    $comment = htmlspecialchars($_POST['comments']);
+    $comments = $comment.'<br>Nombre es: '.$name;
+}
+
+$mail_key = 'fericell2909@gmail.com';   
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = $mail_key;                 // SMTP username
+$mail->Password = 'amenstefano2015';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom($mail_key, 'Marco Estrada');
+$mail->addAddress($email, 'visitante');     // Add a recipient
+$mail->addAddress('info@devmarcoestrada.com', 'visitante');
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$comment = htmlspecialchars($_POST['comments']);
+$comments = $comment.'<br>Nombre is: '.$name/*.'<br>Phone number is: '.$phone*/;
+
+$mail->Subject = 'Pagina devmarcoestrada';
+$mail->Body    = $comments;
+
+if(!$mail->send()) {
+
+    $data = array(
+        'status' => 'Error',
+        'message' => 'Tu mensaje no ha sido enviado. ' . $mail->ErrorInfo
+    );
+} else {
+    $data = array(
+        'status' => 'Congratulation',
+        'message' => 'Tu Mensaje ha sido enviado correctamente. Gracias por contactarte conmigo.'
+    );
+}   
+
 echo json_encode($data);
