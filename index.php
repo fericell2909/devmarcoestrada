77103f065e17ac3a0a3a0e84cea0c6f6b1df696e
Marco Estrada <info@devmarcoestrada.com>
2020-08-09 23:11:11 -0500

    Send Grid

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <meta property="fb:app_id" content="101410998343668" />
     <!--  FavIcon  -->
     
-    <link rel="shortcut icon" type="image/x-icon" href="/assets/img/ico.ico" />
+     <link rel="shortcut icon" type="image/x-icon" href="/assets/img/ico.ico" />
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Muli:400,600" rel="stylesheet">
--- sendEmail.php
+++ sendEmail.php
@@ -1,12 +1,58 @@
 <?php
 
+require 'vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
+
 $data = [];
 
 
+if (isset($_POST['email'])) {
+    $para = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+    $para = filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+}
+
+if (isset($_POST['comments'])) {
+    $comment = htmlspecialchars($_POST['comments']);
+    $comment = $comment.'<br>Nombre es: '.$name;
+}
+
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("info@devmarcoestrada.com", "Marco Estrada");
+$email->setSubject("Mensaje Visitante - " . $name);
+$email->addTo($para, "Visitante");
+$email->addContent(
+    "text/html", $comment);
+
+$sendgrid = new \SendGrid('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');
+try {
+
+
+
+    $response = $sendgrid->send($email);
+
+    $data = array(
+        'status' => 'Congratulation',
+        'message' => 'Tu Mensaje ha sido enviado correctamente. Gracias por contactarte conmigo.');
+
+    
+
+} catch (Exception $e) {
+    
+    $data = array(
+        'status' => 'Error',
+        'message' => 'Tu mensaje no ha sido enviado. ' . $e->getMessage()
+    );
+
+}
+
+/*
 if (isset($_POST['name'])) {
     $name = $_POST['name'];
 }
@@ -46,7 +92,7 @@ $mail->addAddress('info@devmarcoestrada.com', 'visitante');
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $comment = htmlspecialchars($_POST['comments']);
-$comments = $comment.'<br>Nombre is: '.$name/*.'<br>Phone number is: '.$phone*/;
+$comments = $comment.'<br>Nombre is: '.$name/*.'<br>Phone number is: '.$phone;
 
 $mail->Subject = 'Pagina devmarcoestrada';
 $mail->Body    = $comments;
@@ -72,4 +118,5 @@ if(!$mail->send()) {
     );
 }   
 
+*/
 echo json_encode($data);
