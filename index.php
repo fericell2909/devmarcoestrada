fa5c04d65d98bfa0ba6f30d947b2442617fda634
Marco Estrada Lopez <fericell2909@gmail.com>
2020-12-15 00:32:15 -0500

    api whatsapp

--- index.php
+++ index.php
@@ -38,7 +38,26 @@
         <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
     <!--  Custom Style CSS  -->
     <link rel="stylesheet" href="assets/css/style.css">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <style>
+        .float{
+            position:fixed;
+            width:60px;
+            height:60px;
+            bottom:40px;
+            right:40px;
+            background-color:#25d366;
+            color:#FFF;
+            border-radius:50px;
+            text-align:center;
+            font-size:30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index:100;
+        }
+    .my-float{
+        margin-top:16px;
+    }
+    </style>
     </head>
     <body class="pilling-page nill-dark"  data-spy="scroll" data-target="#scrollspy" data-offset="1">
 
@@ -190,7 +209,9 @@
 
     <!--  Page Pilling  Strat  -->
     <div id="pagepiling" class="pagepiling">
-
+    <a href="https://api.whatsapp.com/send?phone=51920200067&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Tus%20Servicios%20" class="float" target="_blank">
+    <i class="fa fa-whatsapp my-float"></i>
+    </a>
         <!--   Header Start -->
         <header>
             <div class="logo-area">
